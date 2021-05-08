03b7dc78e83d44e2b9de122dd094e48f4a2929d9
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-05-08 18:55:50 +0200

    Added premake stuff

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+
+-- Local paths
+local includes = { _SCRIPT_DIR .. "/include" }
+local sources = { _SCRIPT_DIR .. "/include/**", _SCRIPT_DIR .. "/src/**" }
+local outputdir = _SCRIPT_DIR .. "/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Add paths to the higher level instance (top level project)
+for _,v in ipairs(includes) do table.insert(IncludeDirs, v) end
+for _,v in ipairs(sources) do table.insert(SourceFiles, v) end
+table.insert(LibraryDirs, outputdir)
+table.insert(Links, "Library1")
+
+-- Main Project declaration
+project "Library1"
+    kind "StaticLib"
+    language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+    location "build"
+    targetdir(outputdir)
+
+    ---- Main source files
+    includedirs (includes)
+    files (sources)
+
+    -- Organize the files in the Visual Studio project view
+    vpaths {
+        ["include"] = { _SCRIPT_DIR .. "/include/**" },
+        ["src"] = { _SCRIPT_DIR .. "/src/**" }
+    }
+
+    -- Configuration filters, filters are active up to the next filter statement
+    -- Indentation is purely visual
+    filter "configurations:Debug"
+        defines { "DEBUG", "_DEBUG" }
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "On"
+
+    filter { "platforms:Win32" }
+        system "Windows"
+        architecture "x86"
+
+    filter { "platforms:x64" }
+        system "Windows"
+        architecture "x86_64"
