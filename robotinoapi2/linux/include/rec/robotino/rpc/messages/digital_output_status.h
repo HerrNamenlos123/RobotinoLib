b8274efa874551caadb36e0789aa6c3c8e578b7f
me <me@odrive-firmware-vm.localdomain>
2021-10-11 03:27:35 -0700

    Example works

---
+++ robotinoapi2/linux/include/rec/robotino/rpc/messages/digital_output_status.h
@@ -0,0 +1,8 @@
+#ifndef _REC_ROBOTINO_RPC_DIGITAL_OUTPUT_STATUS_H_
+#define _REC_ROBOTINO_RPC_DIGITAL_OUTPUT_STATUS_H_
+
+#include "rec/rpc/serialization/Primitive.h"
+
+DEFINE_PRIMITIVE_TOPICDATA( rec_robotino_rpc_digital_output_status, QVector< bool > )
+
+#endif //_REC_ROBOTINO_RPC_DIGITAL_OUTPUT_STATUS_H_
