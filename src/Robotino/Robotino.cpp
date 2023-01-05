e6cced00cded6120591c8d4a0de1b0a216d32154
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-01-05 19:21:40 +0100

    Add robotino dependencies

---
+++ src/Robotino/Robotino.cpp
@@ -0,0 +1,65 @@
+
+#include "Robotino/Robotino.h"
+#include "Robotino/RobotinoImpl.h"
+
+namespace Robotino {
+
+	// Robotino::RobotinoImplWrapper
+		Robotino::RobotinoImplWrapper::RobotinoImplWrapper() {
+			impl = new RobotinoImpl();
+		}
+
+		Robotino::RobotinoImplWrapper::~RobotinoImplWrapper() {
+			delete impl;
+		}
+
+		RobotinoImpl* Robotino::RobotinoImplWrapper::get() {
+			return impl;
+		}
+	// End Robotino::RobotinoImplWrapper
+
+
+
+
+
+	Robotino::Robotino() {
+		impl = std::make_unique<RobotinoImplWrapper>();
+	}
+
+	void Robotino::Connect(const std::string& ip) {
+		impl->get()->Connect(ip);
+	}
+
+	void Robotino::Disconnect(bool silent) {
+		impl->get()->Disconnect(silent);
+	}
+
+	void Robotino::DriveDirect(float x, float y, float omega) {
+		impl->get()->DriveDirect(x, y, omega);
+	}
+
+	void Robotino::Stop() {
+		impl->get()->Stop();
+	}
+
+	bool Robotino::GetBumper() {
+		return impl->get()->GetBumper();
+	}
+
+	std::vector<double> Robotino::GetPose() {
+		return impl->get()->GetPose();
+	}
+
+	Image Robotino::GetCamera() {
+		return impl->get()->GetCamera();
+	}
+
+	float Robotino::GetDistanceSensor(int index) {
+		return impl->get()->GetDistanceSensor(index);
+	}
+
+	void Shutdown() {
+		rec::robotino::api2::shutdown();
+	}
+
+}
